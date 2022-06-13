1e3de54092eb1385b08739a5985ea0b233c358d8
0xReself <30471941+0xReself@users.noreply.github.com>
2022-06-13 16:55:15 +0200

    Cors fix (#2)
    fix cors issues

--- src/main/java/de/gamestart/java/FlightSystemApplication.java
+++ src/main/java/de/gamestart/java/FlightSystemApplication.java
@@ -10,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -20,6 +23,16 @@ import java.util.List;
 @SpringBootApplication
 public class FlightSystemApplication implements CommandLineRunner {
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+			}
+		};
+	}
+  
 	@Override
 	public void run(String... args) {
 		initData();
